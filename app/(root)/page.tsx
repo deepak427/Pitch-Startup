ad59f8d83b38b20facf56511b9c3ae52fb68a885
Deepak Singh <negid0253@gmail.com>
2025-01-02 10:02:43 +0530

    feat: integrate sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 const Home = async ({
   searchParams,
 }: {
   searchParams: Promise<{ query?: string }>;
 }) => {
-
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      auther: { _id: 1, name: 'Deepak' },
+      _id: 1,
+      desciption: "This is a description",
+      image:
+        "https://spectrum.ieee.org/media-library/image.png?id=26772500&width=1200&height=842",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
 
   return (
     <>
@@ -23,6 +37,20 @@ const Home = async ({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 && (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          )}
+        </ul>
       </section>
     </>
   );
